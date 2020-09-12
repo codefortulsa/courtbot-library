aa9c11d9c99b0f71764ed9c4bcc9d9c843c71a88
jdungan <johnadungan@gmail.com>
2020-09-12 12:02:35 -0500

    init project for posting on pypi.org

---
+++ README.md
@@ -0,0 +1,18 @@
+# Court Scraping utilities
+
+A python library for scraping case information from court systems.
+
+
+
+## Development Install
+
+1. Create and activate a Python 3.7.3 virtual env
+1. `git clone git@github.com:codefortulsa/courtbot-library.git`
+1. `cd courtbot-library`
+1. `pip install -e .`
+
+## Usage
+
+Install with `pip install courtbot`
+
+
---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setup(name='oscn',
+      version='0.0.0.0',
+      description='Generic Court Scraper for Courbot system use',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url='https://github.com/codefortulsa/courtbot-library',
+      author='Code for Tulsa',
+      author_email='code-for-tulsa@googlegroups.com',
+      license='MIT',
+      packages=find_packages(),
+      zip_safe=False,
+      install_requires=[
+          'requests',
+          'beautifulsoup4',
+          ],
+      classifiers=[
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 3.7',
+          ],
+      )
